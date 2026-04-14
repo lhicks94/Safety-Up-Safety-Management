4af6ae16682794ace87b6a4baedf50c8bef67444
lhicks94 <logan@legacysafetymanagement.com>
2026-04-14 09:21:58 -0500

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "20px" }}>
+      <h1>Safety Up</h1>
+      <p>Safety management application – deployment successful.</p>
+    </main>
+  );
+}
+``
