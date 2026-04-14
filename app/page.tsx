4a0890f2de64f445732662069fbccb698a2304b7
lhicks94 <logan@legacysafetymanagement.com>
2026-04-14 10:11:27 -0500

    Delete app directory

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: "20px" }}>
-      <h1>Safety Up</h1>
-      <p>Safety management application – deployment successful.</p>
-    </main>
-  );
-}
-``
