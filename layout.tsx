b542941a7c156ce038509f93261e7ab42d973bcf
lhicks94 <logan@legacysafetymanagement.com>
2026-04-14 09:06:50 -0500

    Delete layout.tsx

--- layout.tsx
+++
@@ -1,8 +0,0 @@
-export const metadata = { title: 'Safety Up' };
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body style={{fontFamily:'Arial',margin:0}}>{children}</body>
-    </html>
-  );
-}
