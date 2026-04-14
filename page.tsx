133b1edf1f2516851f45bac5fc6cbe24fb877993
lhicks94 <logan@legacysafetymanagement.com>
2026-04-14 09:07:25 -0500

    Delete page.tsx

--- page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{padding:'20px'}}>
-      <h1>Safety Up</h1>
-      <p>Welcome to the Safety Up application.</p>
-      <p>This deployment confirms your app is working.</p>
-    </main>
-  );
-}
